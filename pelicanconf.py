1e6760809659b57c5206e366cd4d31b90fb7c131
40439221 <40439221@gm.nfu.edu.tw>
2017-03-07 20:18:57 +0800

    W1 and W2影片整合與零件檔

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'KMOL'
-SITENAME = '2017Spring 協同產品設計實習 (NFU-MDE104B-40423226)'
+SITENAME = '2017Spring 協同產品設計實習 40439221
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
